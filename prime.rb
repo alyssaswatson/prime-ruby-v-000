90755e098c0c091fe1d786fb9218bc8c4f001fe4
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-17 00:40:53 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,9 @@
 def prime?(number)
   if number == 2 || number == 3  
     return true
-  elsif number.even? &&  number % 2 == 0  && number == !2 || number <= 1
+  elsif number.even? &&  number % 2 == 0  && number == !2
+    return false
+  elsif number <= 1
     return false
   elsif number.odd? &&  number % 3 == 0  && number == !3
     return false
