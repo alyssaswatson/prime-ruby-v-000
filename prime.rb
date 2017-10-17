1ceb39591c3cbb1dea920fe91a6fee5cc0d5aaf2
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-17 00:49:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,14 +2,12 @@
 
 def prime?(number)
   counter = 2
-  if number == 2 || number == 3
-    return true
-  elsif number <= 1
+  if number <= 1
     return false
-  elsif
-    while counter < integer
-          if integer % counter == 0
-       return false
-     end
+  end  
+  while counter < integer
+    if integer % counter == 0
+      return false
   end
 end
+
