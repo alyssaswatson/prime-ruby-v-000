b17fe4c21cc1e0f5651bb13752ecef4ba101b7d2
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-17 00:47:30 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,18 @@
 # Add  code here!
 
 def prime?(number)
+  counter = 2
   if number == 2 || number == 3
     return true
   elsif number.even? && number != 2
     return false
   elsif number <= 1
     return false
-  elsif (2...n-1).any?{|i| n % i == 0}
-    return false
+  elsif
+    while counter < integer
+          if integer % counter == 0
+       return false
+     end
   else
     return true
   end
